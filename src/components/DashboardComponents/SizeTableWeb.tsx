884dc915feaf2bb05614d238f11d598477463857
mzakiullahusman <zaki.ullah@codistan.org>
2024-06-03 20:09:12 +0500

    billing web page done (Funding+)

--- src/components/DashboardComponents/SizeTableWeb.tsx
+++ src/components/DashboardComponents/SizeTableWeb.tsx
@@ -22,49 +22,149 @@ const types = [
 
 const tableData = {
   10000: {
-    tradingPeriod: "Unlimited",
-    withdrawals: "Monthly (30 days)",
-    dailyLoss: "$500 (5%)",
-    equityMargin: "$1,000 (10%)",
-    profitTarget: "$1,000 (1% to 5%)",
-    leverage: "1:100 Max",
-    refundableFee: "No Refunds",
+    step1: {
+      tradingPeriod: "Unlimited",
+      minimumTradingDays: "0 days",
+      maxDailyLoss: "$500 (5%)",
+      maxLoss: "$800 (8%)",
+      profitTarget: "$1,000 (10%)",
+      leverage: "1:100 Max",
+      refundableFee: "$100",
+    },
+    step2: {
+      tradingPeriod: "Unlimited",
+      minimumTradingDays: "1 day",
+      maxDailyLoss: "$600 (6%)",
+      maxLoss: "$900 (9%)",
+      profitTarget: "$900 (9%)",
+      leverage: "1:90 Max",
+      refundableFee: "$200",
+    },
+    funded: {
+      tradingPeriod: "1 year",
+      minimumTradingDays: "10 days",
+      maxDailyLoss: "$700 (7%)",
+      maxLoss: "$1000 (10%)",
+      profitTarget: "$1,100 (11%)",
+      leverage: "1:80 Max",
+      refundableFee: "$300",
+    },
   },
   25000: {
-    tradingPeriod: "Unlimited",
-    withdrawals: "Monthly (30 days)",
-    dailyLoss: "$1,000 (5%)",
-    equityMargin: "$2,000 (10%)",
-    profitTarget: "$2,000 (1% to 5%)",
-    leverage: "1:100 Max",
-    refundableFee: "No Refunds",
+    step1: {
+      tradingPeriod: "6 months",
+      minimumTradingDays: "5 days",
+      maxDailyLoss: "$1,000 (4%)",
+      maxLoss: "$1,500 (6%)",
+      profitTarget: "$2,500 (10%)",
+      leverage: "1:100 Max",
+      refundableFee: "$150",
+    },
+    step2: {
+      tradingPeriod: "6 months",
+      minimumTradingDays: "7 days",
+      maxDailyLoss: "$1,200 (4.8%)",
+      maxLoss: "$1,800 (7.2%)",
+      profitTarget: "$2,000 (8%)",
+      leverage: "1:90 Max",
+      refundableFee: "$250",
+    },
+    funded: {
+      tradingPeriod: "1 year",
+      minimumTradingDays: "15 days",
+      maxDailyLoss: "$1,400 (5.6%)",
+      maxLoss: "$2,000 (8%)",
+      profitTarget: "$2,700 (10.8%)",
+      leverage: "1:80 Max",
+      refundableFee: "$350",
+    },
   },
   50000: {
-    tradingPeriod: "Unlimited",
-    withdrawals: "Monthly (30 days)",
-    dailyLoss: "$2,000 (5%)",
-    equityMargin: "$4,000 (10%)",
-    profitTarget: "$4,000 (1% to 5%)",
-    leverage: "1:100 Max",
-    refundableFee: "No Refunds",
+    step1: {
+      tradingPeriod: "3 months",
+      minimumTradingDays: "7 days",
+      maxDailyLoss: "$1,500 (3%)",
+      maxLoss: "$2,000 (4%)",
+      profitTarget: "$5,000 (10%)",
+      leverage: "1:100 Max",
+      refundableFee: "$200",
+    },
+    step2: {
+      tradingPeriod: "3 months",
+      minimumTradingDays: "10 days",
+      maxDailyLoss: "$1,800 (3.6%)",
+      maxLoss: "$2,500 (5%)",
+      profitTarget: "$4,000 (8%)",
+      leverage: "1:90 Max",
+      refundableFee: "$300",
+    },
+    funded: {
+      tradingPeriod: "6 months",
+      minimumTradingDays: "20 days",
+      maxDailyLoss: "$2,000 (4%)",
+      maxLoss: "$3,000 (6%)",
+      profitTarget: "$5,500 (11%)",
+      leverage: "1:80 Max",
+      refundableFee: "$400",
+    },
   },
   100000: {
-    tradingPeriod: "Unlimited",
-    withdrawals: "Monthly (30 days)",
-    dailyLoss: "$5,000 (5%)",
-    equityMargin: "$8,000 (10%)",
-    profitTarget: "$8,000 (1% to 5%)",
-    leverage: "1:100 Max",
-    refundableFee: "No Refunds",
+    step1: {
+      tradingPeriod: "1 month",
+      minimumTradingDays: "10 days",
+      maxDailyLoss: "$2,000 (2%)",
+      maxLoss: "$3,000 (3%)",
+      profitTarget: "$10,000 (10%)",
+      leverage: "1:100 Max",
+      refundableFee: "$300",
+    },
+    step2: {
+      tradingPeriod: "1 month",
+      minimumTradingDays: "15 days",
+      maxDailyLoss: "$2,500 (2.5%)",
+      maxLoss: "$4,000 (4%)",
+      profitTarget: "$8,000 (8%)",
+      leverage: "1:90 Max",
+      refundableFee: "$400",
+    },
+    funded: {
+      tradingPeriod: "3 months",
+      minimumTradingDays: "25 days",
+      maxDailyLoss: "$3,000 (3%)",
+      maxLoss: "$5,000 (5%)",
+      profitTarget: "$11,000 (11%)",
+      leverage: "1:80 Max",
+      refundableFee: "$500",
+    },
   },
   200000: {
-    tradingPeriod: "Unlimited",
-    withdrawals: "Monthly (30 days)",
-    dailyLoss: "$10,000 (5%)",
-    equityMargin: "$16,000 (10%)",
-    profitTarget: "$16,000 (1% to 5%)",
-    leverage: "1:100 Max",
-    refundableFee: "No Refunds",
+    step1: {
+      tradingPeriod: "2 weeks",
+      minimumTradingDays: "14 days",
+      maxDailyLoss: "$4,000 (2%)",
+      maxLoss: "$5,000 (2.5%)",
+      profitTarget: "$20,000 (10%)",
+      leverage: "1:100 Max",
+      refundableFee: "$400",
+    },
+    step2: {
+      tradingPeriod: "2 weeks",
+      minimumTradingDays: "20 days",
+      maxDailyLoss: "$5,000 (2.5%)",
+      maxLoss: "$6,000 (3%)",
+      profitTarget: "$16,000 (8%)",
+      leverage: "1:90 Max",
+      refundableFee: "$500",
+    },
+    funded: {
+      tradingPeriod: "1 month",
+      minimumTradingDays: "30 days",
+      maxDailyLoss: "$6,000 (3%)",
+      maxLoss: "$7,000 (3.5%)",
+      profitTarget: "$22,000 (11%)",
+      leverage: "1:80 Max",
+      refundableFee: "$600",
+    },
   },
 };
 
@@ -203,8 +303,8 @@ const SizeTableWeb = () => {
         <div
           className={`hidden lg:block w-[645px] absolute left-5 h-px -z-10 ${
             selectedSize === 100000
-              ? "top-[17%] w-[645px]"
-              : "top-[22%] w-[630px]"
+              ? "top-[17%] w-[345px]"
+              : "top-[22%] w-[320px]"
           }`}
           style={{ backgroundColor: "#252525" }}
         ></div>
@@ -213,13 +313,22 @@ const SizeTableWeb = () => {
       {/* TABLE  */}
       <div className="w-full mt-6 flex items-start justify-between flex-wrap lg:flex-nowrap">
         <div className="flex flex-col justify-center w-full md:w-[68%] min-h-[669px] py-4 px-6 bg-[#131119] rounded-[14px] text-white border border-[#252525]">
-          <p className="font-[Rubik] font-bold text-[16px] ml-32">
-            FULLY
-            <br /> FUNDED
-          </p>
-          <p className="mb-2 font-[Rubik] font-normal text-[16px] ml-32">
-            Verification
-          </p>
+          <thead>
+            <tr className="flex justify-end md:gap-12 lg:gap-24 2xl:gap-36 md:mr-14 lg:mr-30 2xl:mr-48">
+              <th className="py-5 font-[Rubik] font-bold text-[16px] text-start">
+                Step 1 <br />
+                <span className="text-[#93A1A6] font-normal">Challenge</span>
+              </th>
+              <th className="py-5 font-[Rubik] font-bold text-[16px] text-start">
+                Step 2<br />
+                <span className="text-[#93A1A6] font-normal">Verification</span>
+              </th>
+              <th className="py-5 font-[Rubik] font-bold text-[16px] text-start">
+                Funded <br />
+                <span className="text-[#93A1A6] font-normal">Trader</span>
+              </th>
+            </tr>
+          </thead>
           <table className="w-full">
             <tbody>
               <tr className="border-t border-[#FAF1E01A]">
@@ -227,15 +336,27 @@ const SizeTableWeb = () => {
                   Trading Period
                 </td>
                 <td className="font-[Rubik] font-medium text-[16px] text-start">
-                  {data.tradingPeriod}
+                  {data.step1.tradingPeriod}
+                </td>
+                <td className="font-[Rubik] font-medium text-[16px] text-start">
+                  {data.step2.tradingPeriod}
+                </td>
+                <td className="font-[Rubik] font-medium text-[16px] text-start">
+                  {data.funded.tradingPeriod}
                 </td>
               </tr>
               <tr className="border-t border-[#FAF1E01A]">
                 <td className="py-5 font-[Rubik] font-medium text-[14px] text-start">
-                  Withdrawals (Min. 1%)
+                  Minimum Trading Days
                 </td>
                 <td className="font-[Rubik] font-medium text-[16px] text-start">
-                  {data.withdrawals}
+                  {data.step1.minimumTradingDays}
+                </td>
+                <td className="font-[Rubik] font-medium text-[16px] text-start">
+                  {data.step2.minimumTradingDays}
+                </td>
+                <td className="font-[Rubik] font-medium text-[16px] text-start">
+                  {data.funded.minimumTradingDays}
                 </td>
               </tr>
               <tr className="border-t border-[#FAF1E01A]">
@@ -243,23 +364,41 @@ const SizeTableWeb = () => {
                   Maximum Daily Loss
                 </td>
                 <td className="font-[Rubik] font-medium text-[16px] text-start">
-                  {data.dailyLoss}
+                  {data.step1.maxDailyLoss}
+                </td>
+                <td className="font-[Rubik] font-medium text-[16px] text-start">
+                  {data.step2.maxDailyLoss}
+                </td>
+                <td className="font-[Rubik] font-medium text-[16px] text-start">
+                  {data.funded.maxDailyLoss}
                 </td>
               </tr>
               <tr className="border-t border-[#FAF1E01A]">
                 <td className="py-5 font-[Rubik] font-medium text-[14px] text-start">
-                  Account Equity Margin
+                  Maximum Loss
                 </td>
                 <td className="font-[Rubik] font-medium text-[16px] text-start">
-                  {data.equityMargin}
+                  {data.step1.maxLoss}
+                </td>
+                <td className="font-[Rubik] font-medium text-[16px] text-start">
+                  {data.step2.maxLoss}
+                </td>
+                <td className="font-[Rubik] font-medium text-[16px] text-start">
+                  {data.funded.maxLoss}
                 </td>
               </tr>
               <tr className="border-t border-[#FAF1E01A]">
                 <td className="py-5 font-[Rubik] font-medium text-[14px] text-start">
-                  Monthly Profit Target
+                  Profit Target
                 </td>
                 <td className="font-[Rubik] font-medium text-[16px] text-start">
-                  {data.profitTarget}
+                  {data.step1.profitTarget}
+                </td>
+                <td className="font-[Rubik] font-medium text-[16px] text-start">
+                  {data.step2.profitTarget}
+                </td>
+                <td className="font-[Rubik] font-medium text-[16px] text-start">
+                  {data.funded.profitTarget}
                 </td>
               </tr>
               <tr className="border-t border-[#FAF1E01A]">
@@ -267,7 +406,13 @@ const SizeTableWeb = () => {
                   Account Leverage
                 </td>
                 <td className="font-[Rubik] font-medium text-[16px] text-start">
-                  {data.leverage}
+                  {data.step1.leverage}
+                </td>
+                <td className="font-[Rubik] font-medium text-[16px] text-start">
+                  {data.step2.leverage}
+                </td>
+                <td className="font-[Rubik] font-medium text-[16px] text-start">
+                  {data.funded.leverage}
                 </td>
               </tr>
               <tr className="border-t border-[#FAF1E01A]">
@@ -275,7 +420,13 @@ const SizeTableWeb = () => {
                   Refundable Fee
                 </td>
                 <td className="font-[Rubik] font-medium text-[16px] text-start">
-                  {data.refundableFee}
+                  {data.step1.refundableFee}
+                </td>
+                <td className="font-[Rubik] font-medium text-[16px] text-start">
+                  {data.step2.refundableFee}
+                </td>
+                <td className="font-[Rubik] font-medium text-[16px] text-start">
+                  {data.funded.refundableFee}
                 </td>
               </tr>
             </tbody>
